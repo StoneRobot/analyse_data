023bc577cb64e418a30d612d3962f21eb0567f90
StoneRobot <1107981147@qq.com>
2020-04-15 19:33:16 +0800

    适配之前保存

--- include/analyse_data/enum_cmd.h
+++ include/analyse_data/enum_cmd.h
@@ -7,18 +7,18 @@ class EnumCommand
 {
 public:
     EnumCommand();
-    // virtual ~EnumCommand();
-    int find_intent(std::string str);
-    int find_object(std::string str);
-    int find_target(std::string str); 
+    // int find_intent(std::string str);
+    // int find_object(std::string str);
+    // int find_target(std::string str); 
+    std::string find_intent(std::string);
+    std::string find_object(std::string);
+    std::string find_target(std::string);
 
 private:
     enum intent
     {
         replace,
-        place,
-        // begin,
-        // stop
+        place
     };
     enum object
     {
@@ -30,17 +30,10 @@ private:
         
         shelf,
         shelf_first,
-        table,
+        table
     };
-    // typedef std::multimap<std::string, intent> Intent_map;
-    // std::multimap<std::string, intent> Intent;
-    // Intent.insert(std::multimap<std::string, intent>::value_type("place", intent::place));
-
-    //{"place" : intent::place, "replace" : intent::replace, "begin" : intent::begin, "stop" : intent::stop};
     Json::Value Intent; 
-    // {"牛奶盒" : object::milk, "可乐罐" : object::coke};
     Json::Value Object;
-    //{"架子" : target::shelf, "桌子" : target::table};
     Json::Value Target; 
 };
 
--- src/enum_cmd.cpp
+++ src/enum_cmd.cpp
@@ -1,29 +1,57 @@
 #include "analyse_data/enum_cmd.h"
 
+// EnumCommand::EnumCommand()
+// {
+//     Intent["place"] = Json::Value(intent::place);
+//     Intent["replace"] = Json::Value(intent::replace);
+//     // Intent["begin"] = Json::Value(intent::begin);
+//     // Intent["stop"] = Json::Value(intent::stop);
+
+//     Object["牛奶盒"] = Json::Value(object::milk);
+//     Object["可乐罐"] = Json::Value(object::coke);
+
+//     Target["架子"] = Json::Value(target::shelf);
+//     Target["架子第一层"] = Json::Value(target::shelf_first);
+//     Target["桌子"] = Json::Value(target::table);
+// }
+
+// int EnumCommand::find_intent(std::string str)
+// {
+//     return Intent[str].asInt();
+// }
+// int EnumCommand::find_object(std::string str)
+// {
+//     return Object[str].asInt();
+// }
+// int EnumCommand::find_target(std::string str)
+// {
+//     return Target[str].asInt();
+// }
+
 EnumCommand::EnumCommand()
 {
-    Intent["place"] = Json::Value(intent::place);
-    Intent["replace"] = Json::Value(intent::replace);
+    Intent["place"] = Json::Value("place");
+    Intent["replace"] = Json::Value("replace");
     // Intent["begin"] = Json::Value(intent::begin);
     // Intent["stop"] = Json::Value(intent::stop);
 
-    Object["牛奶盒"] = Json::Value(object::milk);
-    Object["可乐罐"] = Json::Value(object::coke);
+    Object["牛奶盒"] = Json::Value("milk");
+    Object["可乐罐"] = Json::Value("coke");
 
-    Target["架子"] = Json::Value(target::shelf);
-    Target["架子第一层"] = Json::Value(target::shelf_first);
-    Target["桌子"] = Json::Value(target::table);
+    Target["架子"] = Json::Value("shelf");
+    Target["架子第一层"] = Json::Value("shelf_first");
+    Target["桌子"] = Json::Value("table");
 }
 
-int EnumCommand::find_intent(std::string str)
+std::string EnumCommand::find_intent(std::string str)
 {
-    return Intent[str].asInt();
+    return Intent[str].asString();
 }
-int EnumCommand::find_object(std::string str)
+std::string EnumCommand::find_object(std::string str)
 {
-    return Object[str].asInt();
+    return Object[str].asString();
 }
-int EnumCommand::find_target(std::string str)
+std::string EnumCommand::find_target(std::string str)
 {
-    return Target[str].asInt();
+    return Target[str].asString();
 }
